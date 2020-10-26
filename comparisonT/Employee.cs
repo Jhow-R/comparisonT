e8dff3199cb56b77d9ac50cdedccbee410697774
Jhonathan Araujo <jhonathanaraujobr@hotmail.com>
2020-10-26 13:42:05 -0300

    Inclusão da classe de funcionário

---
+++ comparisonT/Employee.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace comparisonT
+{
+    class Employee : IComparable<Employee>
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public int CompareTo(Employee other)
+        {
+            // Se o salário for igual então faz a ordenação alfabética [A a Z]
+            if (this.Salary == other.Salary)
+                return other.Name.CompareTo(Name);
+
+            // Ordenação padrão: do maior para o menor salário
+            return other.Salary.CompareTo(Salary);
+        }
+
+        public override string ToString()
+        {
+            return this.Salary.ToString("C") + " - " + this.Name;
+        }
+    }
+}
--- comparisonT/Program.cs
+++ comparisonT/Program.cs
@@ -7,29 +7,46 @@ namespace comparisonT
     {
         static void Main(string[] args)
         {
-            List<Product> list = new List<Product>();
+            List<Product> products = new List<Product>();
 
-            list.Add(new Product("TV", 900.00));
-            list.Add(new Product("Notebook", 1200.00));
-            list.Add(new Product("Tablet", 450.00));
+            products.Add(new Product("TV", 900.00));
+            products.Add(new Product("Notebook", 1200.00));
+            products.Add(new Product("Tablet", 450.00));
 
             // Ordenação com CompareTo (classe implementa IComparable<T>)
-            list.Sort();
+            products.Sort();
 
             // Ordenação com Comparison<T> (delegate)
             Comparison<Product> comparison1 = CompareProducts;
-            list.Sort(comparison1);
+            products.Sort(comparison1);
             // Ou
-            list.Sort(CompareProducts);
+            products.Sort(CompareProducts);
 
             // Ordenação usando Lambda
             Comparison<Product> comparison2 = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
-            list.Sort(comparison2);
+            products.Sort(comparison2);
             // Ou
-            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+            products.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
 
-            foreach (Product p in list)
+            foreach (Product p in products)
                 Console.WriteLine(p);
+
+            Console.Write(Environment.NewLine);
+
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee() { Name = "Macoratti", Salary = 10000 });
+            employees.Add(new Employee() { Name = "Janice", Salary = 10000 });
+            employees.Add(new Employee() { Name = "Yuri", Salary = 40000 });
+            employees.Add(new Employee() { Name = "Bianca", Salary = 10000 });
+            employees.Add(new Employee() { Name = "Amanda", Salary = 500000 });
+            employees.Add(new Employee() { Name = "Jefferson", Salary = 17000 });
+            employees.Add(new Employee() { Name = "Dinora", Salary = 8000 });
+
+            // Ordem invertida no método CompareTo() da classe Employee
+            employees.Sort();
+
+            foreach (var funcionario in employees)
+                Console.WriteLine(funcionario);
 
             Console.ReadLine();
         }
